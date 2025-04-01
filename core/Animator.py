32ad05b8918d0818b6203fe2cd1c3675dd3d772d
Krakken13 <egordragon13@gmail.com>
2025-04-01 23:47:48 +0700

    HOTFIX 1

--- core/Animator.py
+++ core/Animator.py
@@ -24,7 +24,7 @@ class Animator:
         for frame in sorted(path.iterdir(), key=extract_number):
             self.animations[folder].append(pg.image.load(frame).convert_alpha())
 
-    def animate(self, dt: int, flip_x: bool = False, flip_y: bool = False, loop: bool = False) -> pg.Surface:
+    def animate(self, dt: int, flip_x=False, flip_y=False, loop=True, reverse=False) -> pg.Surface:
         if not self.active or self.active not in self.animations:
             return pg.Surface((1, 1), pg.SRCALPHA)
 
@@ -32,17 +32,19 @@ class Animator:
             self.animation_timer += dt
             if self.animation_timer >= self.animation_speed:
                 self.animation_timer = 0
-                self.current_frame += 1
+                self.current_frame += -1 if reverse else 1
                 if self.current_frame >= len(self.animations[self.active]):
                     self.current_frame = 0 if loop else len(self.animations[self.active]) - 1
+                elif self.current_frame < 0:
+                    self.current_frame = len(self.animations[self.active]) - 1 if loop else 0
         return pg.transform.flip(self.animations[self.active][self.current_frame], flip_x, flip_y)
 
     def set(self, animation_type: str):
-        if animation_type in self.animations:
+        if self.has_animation(animation_type):
             self.active = animation_type
             self.reset()
         elif not self.active:
-            self.active = next(iter(self.animations), None)
+            self.active = next(iter(self.animations))
         else:
             print(f"[Animator] couldn't find animation {animation_type}")
 
@@ -66,6 +68,9 @@ class Animator:
     def stop(self):
         self.reset()
         self.pause()
+
+    def has_animation(self, animation_type: str) -> bool:
+        return animation_type in self.animations
 
     def is_playing(self) -> bool:
         return not self.paused
